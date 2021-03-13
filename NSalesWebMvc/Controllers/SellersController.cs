9c9741adb8db4a075cdbdd99833823f4ad158de1
Thiago Turcato do Rego <turcato1@uol.com.br>
2021-03-13 15:19:04 -0300

    Delete Seller

--- NSalesWebMvc/Controllers/SellersController.cs
+++ NSalesWebMvc/Controllers/SellersController.cs
@@ -15,12 +15,7 @@ namespace NSalesWebMvc.Controllers
             _sellersService = sellerService;
             _departmentService = departmentService;
         }
-
-        //public SellersController(SellerService sellersService)
-        //{
-        //    _sellersService = sellersService;
-        //}
-
+          
         public IActionResult Index()
         {
             var list = _sellersService.FindAll();
@@ -41,5 +36,30 @@ namespace NSalesWebMvc.Controllers
             _sellersService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellersService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellersService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
