645250b4567b5d5956f4d3fe2371213ff966013e
Thiago Turcato do Rego <turcato1@uol.com.br>
2021-03-13 13:44:17 -0300

    Foreign Key not null

--- NSalesWebMvc/Migrations/20210313163821_DepartmentForeignKey.Designer.cs
+++ NSalesWebMvc/Migrations/20210313163821_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSalesWebMvc.Data;
 
 namespace NSalesWebMvc.Migrations
 {
     [DbContext(typeof(NSalesWebMvcContext))]
-    partial class NSalesWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20210313163821_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
